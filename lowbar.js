db3df9881fd360b0f1bfb43b36acd1bc24dc7003
Hoofman <hoofman@sdf-eu.org>
2016-10-21 13:10:07 +0100

    _.first complete, 11 tests pass

--- lowbar.js
+++ lowbar.js
@@ -1,16 +1,35 @@
 var _ = require('underscore')
 
 
-
 var _ = {};
 
-_.identity = function(x) {
-  return x;
+_.identity = function (x) {
+    return x;
 };
 
 
+_.first = function (array, num) {
 
+    if (array == null) {
+        return 0;
+    }
+    else if (!num) {
+        return array[0];
+    }
+    else {
+        (num > array.length)
+        {
+            num = array.length
+        }
+        console.log(num);
+        var newArray = [];
+        for (i = 0; i < num; i++) {
+            newArray.push(array[i]);
+        }
+        return newArray;
+    }
+};
 
 if (typeof module !== 'undefined') {
-  module.exports = _;
+    module.exports = _;
 }
--- spec/lowbarspec.js
+++ spec/lowbarspec.js
@@ -23,6 +23,48 @@ describe('_', function () {
     expect(_.identity('x')).to.equal('x');
   });
 
+  it('_.first is a function', function () {
+    expect(_.first).to.be.a('function');
+  });
+
+  it('_.first should take a least one argument', function () {
+    expect(_.first.length).to.be.least(1);
+  });
+
+  it('_.first([5,4,3,2,1]) should return 5', function () {
+    expect(_.first([5,4,3,2,1])).to.equal(5);
+  });
+
+  it('_.first([5,4,3,2,1], 3) should return [5,4,3]', function () {
+    expect(_.first([5,4,3,2,1],3)).to.eql([5,4,3]);
+  });
+
+  it('_.first passed no arguments should return 0', function () {
+    expect(_.first()).to.equal(0);
+  });
+
+  it('_.first var array = var expected', function () {
+    var array = [1,2,3];
+    var expected = [1,2,3];
+    expect(_.first(array,8)).to.eql(expected);
+  });
+
+
+
+
+
+
+
+
+  /*
+
+    it('_.first should take an array as first argument', function () {
+      expect(_.first([0])).to.equal('object');
+    });
+  */
+
+
+
   describe('#each', function () {
     it('is a function', function() {
       expect(_.each).to.be.a('function');
