90b1667de1c1a1377185090b07198cee22f37d5f
benjicwood <bencharlwood@gmail.com>
2016-10-24 11:55:44 +0100

    indexOf works 8 passes

--- lowbar.js
+++ lowbar.js
@@ -1,6 +1,5 @@
 //var _ = require('underscore')
 
-
 var _ = {};
 
 _.identity = function (x) {
@@ -9,50 +8,58 @@ _.identity = function (x) {
 
 
 _.first = function (array, num) {
-
-    if (array == null) {
-        return 0;
-    }
-    if (!num) {
-        return array[0];
-    }
-    if
-        (num > array.length) {
-            num = array.length
-        }
-        var newArray = [];
-        for (i = 0; i < num; i++) {
-            newArray.push(array[i]);
-        }
-        return newArray;
-    };
+  if (array == null) {  return 0;  }
+  if (!num) {  return array[0];    }
+  if (num > array.length) {
+      num = array.length  }
+  var newArray = [];
+  for (i = 0; i < num; i++) {
+  newArray.push(array[i]);
+  }
+  return newArray;
+};
 
 _.last = function (array, num) {
-  if (array == null) {
-      return 0;
-}
-  if (num == 0) {
-    return [];
-  };
-
-  if (!num) {
-    return array[array.length-1];
-  }
-
+  if (array == null) {  return 0; }
+  if (num == 0) {  return []; };
+  if (!num) { return array[array.length-1]; }
   var newArray = array.slice(array.length-num, array.length);
-
   return newArray;
 };
 
 _.each = function(array, func) {
-    if(!array) {
-        return;
-    };
-
-    for (i = 0; i < array.length; i++) {
-        func(array[i]);
+  if(!array) {  return;  };
+  for (i = 0; i < array.length; i++) {
+  func(array[i]);
     };
 };
+
+_.indexOf = function (num, array, isSorted) {
+  if(!array) { return -1 }
+  if(typeof array !== 'object') { return -1 }
+  var sort = 0
+  if (isSorted == 'isSorted') {
+    for (var i = 0; i < array.length; i++) {
+    if (array[i] >= array[i+1])
+     { sort = 1; }
+//return false;
+  }
+  if (sort == 0) {
+    return true
+  }
+}
+  for (var i = 0; i < array.length; i++) {
+  if (array[i] == num) {
+  return i
+    };
+};
+
+};
+
+_.filter = function () {
+
+};
+
 
 
 if (typeof module !== 'undefined') {
--- spec/lowbarspec.js
+++ spec/lowbarspec.js
@@ -10,116 +10,141 @@ describe('_', function () {
     it('is an object', function () {
         expect(_).to.be.an('object');
     });
-    // _.identity
-    it('_.identity is a function', function () {
-        expect(_.identity).to.be.a('function');
-    });
-    it('_.identity takes one argument', function () {
-        expect(_.identity.length).to.equal(1);
-    });
-    it('_.identity should not return undefined if given an argument of x', function () {
-        expect(_.identity('x')).to.not.equal(undefined);
-    });
-    it('_.identity should return the unmodified argument', function () {
-        expect(_.identity('x')).to.equal('x');
-    });
-// _.first
-    it('_.first is a function', function () {
-        expect(_.first).to.be.a('function');
-    });
-
-    it('_.first should take a least one argument', function () {
-        expect(_.first.length).to.be.least(1);
-    });
-
-    it('_.first([5,4,3,2,1]) should return 5', function () {
-        expect(_.first([5, 4, 3, 2, 1])).to.equal(5);
-    });
-
-    it('_.first([5,4,3,2,1], 3) should return [5,4,3]', function () {
-        expect(_.first([5, 4, 3, 2, 1], 3)).to.eql([5, 4, 3]);
-    });
-
-    it('_.first passed no arguments should return 0', function () {
-        expect(_.first()).to.equal(0);
-    });
-
-    it('_.first var array = var expected', function () {
-        var array = [1, 2, 3];
-        var expected = [1, 2, 3];
-        expect(_.first(array, 8)).to.eql(expected);
-    });
-    // _.last
-    it('_.last is a function', function () {
-        expect(_.last).to.be.a('function');
-    });
-    it('_.last should take a least one argument', function () {
-        expect(_.last.length).to.be.least(1);
-    });
-    it('_.last passed no arguments should return 0', function () {
-        expect(_.last()).to.equal(0);
-    });
-    it('_.last([5, 4, 3, 2, 1], 3) should return [3, 2, 1]', function () {
-        expect(_.last([5, 4, 3, 2, 1], 3)).to.eql([3, 2, 1]);
-    });
-    it('_.last([5, 4, 3, 2, 1]) should return 1', function () {
-        expect(_.last([5, 4, 3, 2, 1])).to.equal(1);
-    });
-    it('_.last([5, 4, 3, 2, 1], 3) should return [5, 4, 3, 2, 1]', function () {
-        expect(_.last([5, 4, 3, 2, 1], 10)).to.eql([5, 4, 3, 2, 1]);
-    });
-    it('_.last([5, 4, 3, 2, 1], num>0) should return an array', function () {
-        expect(_.last([5, 4, 3, 2, 1], 1)).to.be.a('array');
-    });
-    it('_.last num value is 0 should return an empty array', function () {
-        expect(_.last([5, 4, 3, 2, 1], 0)).to.eql([]);
-    });
-// _.each
-    it('_.each is a function', function () {
-        expect(_.each).to.be.a('function');
-    });
-    it('_.each should take at least 2 arguments', function () {
-        expect(_.each.length).to.be.least(2);
-    });
-
-    it('_.each test as doubler', function () {
-        var inputArray=[1,2,3];
-        var newArray=[];
-        _.each(inputArray,function(arrayElement){newArray.push(arrayElement*2);})
-        expect(newArray).to.eql([2,4,6]);
-    });
-
-    it('_.each test as tripler', function () {
-        var inputArray=[1,2,3];
-        var newArray=[];
-        _.each(inputArray,function(arrayElement){newArray.push(arrayElement*3);})
-        expect(newArray).to.eql([3,6,9]);
-    });
-
-    it('_.each test as quadrupler', function () {
-        var inputArray=[1,2,3];
-        var newArray=[];
-        _.each(inputArray,function(arrayElement){newArray.push(arrayElement*4);})
-        expect(newArray).to.eql([4,8,12]);
-    });
-
-    it('_.each does nothing empty array', function () {
-        var inputArray=[];
-        var newArray=[];
-        _.each(inputArray,function(arrayElement){newArray.push(arrayElement*4);})
-        expect(newArray).to.eql([]);
-    });
-
-    it('_.each runs without function', function () {
-        var inputArray=[];
-        expect(_.each(inputArray));
-    });
-    it('_.each exits gracefully when no args passed ', function () {
-        expect(_.each());
-    });
-
+  });
+  // _.identity
+describe('_.identity', function () {
+  it('_.identity is a function', function () {
+      expect(_.identity).to.be.a('function');
+  });
+  it('_.identity takes one argument', function () {
+      expect(_.identity.length).to.equal(1);
+  });
+  it('_.identity should not return undefined if given an argument of x', function () {
+      expect(_.identity('x')).to.not.equal(undefined);
+  });
+  it('_.identity should return the unmodified argument', function () {
+      expect(_.identity('x')).to.equal('x');
+  });
 });
-
-
-
-
+// _.first
+describe('_.first', function () {
+  it('_.first is a function', function () {
+      expect(_.first).to.be.a('function');
+  });
+  it('_.first should take a least one argument', function () {
+      expect(_.first.length).to.be.least(1);
+  });
+  it('_.first([5,4,3,2,1]) should return 5', function () {
+      expect(_.first([5, 4, 3, 2, 1])).to.equal(5);
+  });
+  it('_.first([5,4,3,2,1], 3) should return [5,4,3]', function () {
+      expect(_.first([5, 4, 3, 2, 1], 3)).to.eql([5, 4, 3]);
+  });
+  it('_.first passed no arguments should return 0', function () {
+      expect(_.first()).to.equal(0);
+  });
+  it('_.first var array = var expected', function () {
+      var array = [1, 2, 3];
+      var expected = [1, 2, 3];
+      expect(_.first(array, 8)).to.eql(expected);
+  });
+});
+//_.last
+describe('_.last', function() {
+   it('_.last is a function', function () {
+      expect(_.last).to.be.a('function');
+  });
+  it('_.last should take a least one argument', function () {
+      expect(_.last.length).to.be.least(1);
+  });
+  it('_.last passed no arguments should return 0', function () {
+      expect(_.last()).to.equal(0);
+  });
+  it('_.last([5, 4, 3, 2, 1], 3) should return [3, 2, 1]', function () {
+      expect(_.last([5, 4, 3, 2, 1], 3)).to.eql([3, 2, 1]);
+  });
+  it('_.last([5, 4, 3, 2, 1]) should return 1', function () {
+      expect(_.last([5, 4, 3, 2, 1])).to.equal(1);
+  });
+  it('_.last([5, 4, 3, 2, 1], 10) should return [5, 4, 3, 2, 1]', function () {
+      expect(_.last([5, 4, 3, 2, 1], 10)).to.eql([5, 4, 3, 2, 1]);
+  });
+  it('_.last([5, 4, 3, 2, 1], num>0) should return an array', function () {
+      expect(_.last([5, 4, 3, 2, 1], 1)).to.be.a('array');
+  });
+  it('_.last num value is 0 should return an empty array', function () {
+      expect(_.last([5, 4, 3, 2, 1], 0)).to.eql([]);
+  });
+});
+// _.each
+describe('_.each', function () {
+  it('_.each is a function', function () {
+      expect(_.each).to.be.a('function');
+  });
+  it('_.each should take at least 2 arguments', function () {
+      expect(_.each.length).to.be.least(2);
+  });
+  it('_.each test as doubler', function () {
+      var inputArray=[1,2,3];
+      var newArray=[];
+      _.each(inputArray,function(arrayElement){newArray.push(arrayElement*2);})
+      expect(newArray).to.eql([2,4,6]);
+  });
+  it('_.each test as tripler', function () {
+      var inputArray=[1,2,3];
+      var newArray=[];
+      _.each(inputArray,function(arrayElement){newArray.push(arrayElement*3);})
+      expect(newArray).to.eql([3,6,9]);
+  });
+  it('_.each test as quadrupler', function () {
+      var inputArray=[1,2,3];
+      var newArray=[];
+      _.each(inputArray,function(arrayElement){newArray.push(arrayElement*4);})
+      expect(newArray).to.eql([4,8,12]);
+  });
+  it('_.each does nothing empty array', function () {
+      var inputArray=[];
+      var newArray=[];
+      _.each(inputArray,function(arrayElement){newArray.push(arrayElement*4);})
+      expect(newArray).to.eql([]);
+  });
+  it('_.each runs without function', function () {
+      var inputArray=[];
+      expect(_.each(inputArray));
+  });
+  it('_.each exits gracefully when no args passed ', function () {
+      expect(_.each());
+  });
+});
+//_.indexOf
+describe('_.indexOf', function () {
+  it('is a function', function () {
+    expect(_.indexOf).to.be.a('function');
+  });
+  it('takes three arguments', function () {
+    expect(_.indexOf.length).to.equal(3);
+  });
+  it('returns -1 if argument isn\'t an array', function () {
+    expect(_.indexOf('hello')).to.equal(-1);
+    expect(_.indexOf(1)).to.equal(-1);
+  });
+  it('returns the index number of a given number from an array of numbers', function () {
+    expect(_.indexOf(2, [1,2,3])).to.eql(1);
+  });
+  it('returns the index number of a given string from an array of strings', function () {
+    expect(_.indexOf('a', ['a', 'b', 'c'])).to.eql(0);
+    expect(_.indexOf('c', ['a', 'b', 'c'])).to.eql(2);
+  });
+  it('returns true if array is sorted', function () {
+    expect(_.indexOf(1, [1,2,3], 'isSorted')).to.equal(true);
+  });
+  it('returns the index of given value if array is unsorted', function () {
+    expect(_.indexOf(2, [1,2,3,2,1], 'isSorted')).to.equal(1);
+  });
+});
+//_.filter
+describe('_.filter', function () {
+  it('is a function', function () {
+    expect(_.filter).to.be.a('function');
+  });
+});
